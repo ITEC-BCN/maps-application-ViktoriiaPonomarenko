33554bb6e1012db3d10ef9d48abc74c7906b48c7
ViktoriiaPonomarenko <viktoriia.ponomarenko.7e6@itb.cat>
2025-05-16 14:03:03 +0200

    .

--- app/src/main/java/com/example/mapsapp/SupabaseApplication.kt
+++ app/src/main/java/com/example/mapsapp/SupabaseApplication.kt
@@ -1,14 +1,17 @@
 package com.example.mapsapp
 
 import android.app.Application
+import com.example.mapsapp.data.MySupabaseClient
 import com.example.mapsapp.data.SupabaseManager
 
 class SupabaseApplication: Application() {
     companion object{
+        lateinit var database: MySupabaseClient
         lateinit var supabase: SupabaseManager
     }
     override fun onCreate() {
         super.onCreate()
         supabase = SupabaseManager()
+        database = MySupabaseClient()
     }
 }
